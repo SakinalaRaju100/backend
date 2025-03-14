ff75d97fce2a9f34c1b1b3dc0db78aa7903d0402
raju.sakinala <raju.sakinala@apollohl.com>
2025-03-14 11:28:27 +0530

    Updated

--- controllers/taxi.js
+++ controllers/taxi.js
@@ -2,30 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
+const taxiSchema = new mongoose.Schema(
+  {
+    // name: { type: String, required: true },
+    // email: { type: String, required: true, unique: true },
+    // password: { type: String, required: true },
+  },
+  { strict: false, timestamps: true }
+);
+
+const Taxies = mongoose.model("Taxies", taxiSchema, "Taxies");
+
+const passengerSchema = new mongoose.Schema(
+  {
+    // name: { type: String, required: true },
+    // email: { type: String, required: true, unique: true },
+    // password: { type: String, required: true },
+  },
+  { strict: false, timestamps: true }
+);
+
+const Passengers = mongoose.model("Passengers", passengerSchema, "Passengers");
+
 let db = null;
-
-// const connectDB = () => {
-//   try {
-//     mongoose.connect(
-//       "mongodb+srv://sakinalaraju100:ObBamLOL3fm9X16z@cluster0.4bvgg.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0?tls=true",
-//       {
-//         // useNewUrlParser: true,
-//         // useUnifiedTopology: true,
-//         connectTimeoutMS: 30000,
-//       }
-//     );
-
-//     db = mongoose.connection;
-//     if (db) {
-//       db.on("error", console.error.bind(console, "connection error:"));
-//       db.once("open", () => {
-//         console.log("Connected to MongoDB");
-//       });
-//     }
-//   } catch (er) {
-//     console.log("er", er);
-//   }
-// };
 
 const connectDB = () => {
   return new Promise((resolve, reject) => {
@@ -59,14 +58,131 @@ const connectDB = () => {
   });
 };
 
-router.get("/", async (req, res) => {
+router.post("/get-taxies", async (req, res) => {
   if (!db || db.readyState !== 1) {
     // Check if db is not connected
     await connectDB(); // Ensure the database connection is established
   }
-  const collection = db.collection("taxies"); // Replace with your collection name
-  const data = await collection.find({}).toArray();
+
+  const data = await Taxies.find();
   res.send(data);
+});
+router.post("/get-passengers", async (req, res) => {
+  if (!db || db.readyState !== 1) {
+    // Check if db is not connected
+    await connectDB(); // Ensure the database connection is established
+  }
+
+  const data = await Passengers.find();
+  res.send(data);
+});
+router.post("/add-taxi", async (req, res) => {
+  if (!db || db.readyState !== 1) {
+    // Check if db is not connected
+    await connectDB(); // Ensure the database connection is established
+  }
+
+  const {
+    location = [],
+    name = "NA",
+    seats = 1,
+    from,
+    to,
+    mobile,
+    vehicleNumber = "NA",
+    role = "taxi",
+  } = req.body;
+  if (!mobile || !from || !to) {
+    return res.status(201).send("data missing"); // Respond with the saved taxi data
+  }
+
+  try {
+    const saveObj = {
+      location,
+      name,
+      seats,
+      from,
+      to,
+      mobile,
+      vehicleNumber,
+      role,
+    };
+
+    const result = await Taxies.updateOne(
+      {
+        mobile,
+      }, // Filter to find the document
+      {
+        $set: {
+          ...saveObj,
+        },
+      }, // Update this field
+      { upsert: true } // If not found, create a new document
+    );
+
+    res.status(201).send({ success: true, message: "Operation successful" }); // Respond with the saved taxi data
+  } catch (error) {
+    console.error("Error adding taxi:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Operation failed", error }); // Handle errors
+  }
+});
+router.post("/add-passenger", async (req, res) => {
+  if (!db || db.readyState !== 1) {
+    // Check if db is not connected
+    await connectDB(); // Ensure the database connection is established
+  }
+
+  const {
+    location = [],
+    name = "NA",
+    // seats = 1,
+    from,
+    to,
+    mobile,
+    // vehicleNumber = "NA",
+
+    members = 1,
+    role = "passenger",
+  } = req.body;
+  if (!mobile || !from || !to) {
+    return res.status(201).send("data missing"); // Respond with the saved taxi data
+  }
+
+  try {
+    const saveObj = {
+      location,
+      name,
+      //   seats,
+      from,
+      to,
+      mobile,
+      //   vehicleNumber,
+
+      members,
+      role,
+    };
+
+    const result = await Passengers.updateOne(
+      {
+        mobile,
+      }, // Filter to find the document
+      {
+        $set: {
+          ...saveObj,
+        },
+      }, // Update this field
+      { upsert: true } // If not found, create a new document
+    );
+
+    res.status(201).send({ success: true, message: "Operation successful" }); // Respond with the saved taxi data
+  } catch (error) {
+    console.error("Error adding taxi:", error);
+    res
+      .status(500)
+      .send({ success: false, message: "Operation failed", error }); // Handle errors
+  }
 });
 
 module.exports = router;
